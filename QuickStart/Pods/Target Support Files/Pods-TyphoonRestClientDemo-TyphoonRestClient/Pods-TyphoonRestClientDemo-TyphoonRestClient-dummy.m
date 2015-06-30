e811d806db7dbdc2653b191c8448ed7e9c4a08b2
Aleksey Garbarev <alex.garbarev@gmail.com>
2015-06-30 20:35:18 +0600

    Initial commit

---
+++ QuickStart/Pods/Target Support Files/Pods-TyphoonRestClientDemo-TyphoonRestClient/Pods-TyphoonRestClientDemo-TyphoonRestClient-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TyphoonRestClientDemo_TyphoonRestClient : NSObject
+@end
+@implementation PodsDummy_Pods_TyphoonRestClientDemo_TyphoonRestClient
+@end
---
+++ TyphoonRestClientDemoSwift/Pods/Target Support Files/Pods-TyphoonRestClientDemoSwift-TyphoonRestClient/Pods-TyphoonRestClientDemoSwift-TyphoonRestClient-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TyphoonRestClientDemoSwift_TyphoonRestClient : NSObject
+@end
+@implementation PodsDummy_Pods_TyphoonRestClientDemoSwift_TyphoonRestClient
+@end
